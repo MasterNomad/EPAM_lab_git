3e3a56682044c529bfc487b7e925ae946b2460cc
Sozonov Anton <Unboundnomad@gmail.com>
2018-11-20 11:17:38 +0400

    Create - gHappy

---
+++ src/String_3/gHappy.java
@@ -0,0 +1,35 @@
+/*
+We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. Return
+true if all the g's in the given string are happy.
+
+https://codingbat.com/prob/p198664
+ */
+
+package String_3;
+
+public class gHappy {
+
+    public boolean start(final String str) {
+
+        if (str.equals("g")) {
+            return false;
+        }
+
+        int index = str.indexOf('g',1);
+
+        while (index > 0) {
+
+            if (index == str.length()-1 && str.charAt(index - 1) != 'g') {
+                return false;
+            }
+            if (str.charAt(index - 1) != 'g' && str.charAt(index + 1) != 'g') {
+                return false;
+            }
+
+            index = str.indexOf(str.charAt(index), ++index);
+        }
+
+        return true;
+
+    }
+}
