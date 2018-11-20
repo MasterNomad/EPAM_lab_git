177e4c62d6dafc3a54de0c81eb84cd4c8f4a26dd
Sozonov Anton <Unboundnomad@gmail.com>
2018-11-20 11:03:12 +0400

    Create - equallsNot

---
+++ src/String_3/equalIsNot.java
@@ -0,0 +1,33 @@
+/*
+Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances
+of "not" anywhere in the string (case sensitive).
+
+https://codingbat.com/prob/p141736
+ */
+
+package String_3;
+
+public class equalIsNot {
+
+    public boolean start(final String str) {
+
+        return substringCounter(str, "is") == substringCounter(str, "not");
+
+    }
+
+    private int substringCounter(final String str, final String search) {
+
+        int index = str.indexOf(search);
+
+        int result = 0;
+
+        while (index != -1) {
+            result++;
+            index = str.indexOf(search, ++index);
+        }
+
+        return result;
+
+    }
+
+}
