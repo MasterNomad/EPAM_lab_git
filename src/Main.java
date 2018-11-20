448051ad25c094035d63e64b46a8f60593724a4e
Sozonov Anton <Unboundnomad@gmail.com>
2018-11-20 10:34:25 +0400

    Create - Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import String_3.countYZ;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Считает y или z в конце слова в строке \"y2bz !!day--yaz!! DAY abc XYZ\"");
+        System.out.println(new countYZ().start("y2bz !!day--yaz!! DAY abc XYZ"));
+    }
+
+}
