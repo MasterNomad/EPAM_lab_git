fb8cdb4e28d20ae2cbdd0e15b599d38561c79228
Sozonov Anton <Unboundnomad@gmail.com>
2018-11-20 10:48:04 +0400

    Main add - withoutString

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import String_3.countYZ;
+import String_3.withoutString;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Считает y или z в конце слова в строке \"y2bz !!day--yaz!! DAY abc XYZ\"");
+
+        System.out.println("Считает 'y' и 'z' в конце слова в строке \"y2bz !!day--yaz!! DAY abc XYZ\"");
         System.out.println(new countYZ().start("y2bz !!day--yaz!! DAY abc XYZ"));
+        System.out.println();
+
+        System.out.println("Удалет из строки \"MkjtMkx\" строку \"Mk\"");
+        System.out.println(new withoutString().start("MkjtMkx", "Mk"));
+        System.out.println();
     }
 
 }
