e49af0a24de721a784b9550762c829b9b0fc9c72
Anton Sozonov <unboundnomad@gmail.com>
2019-04-10 12:41:39 +0400

    Create - NewMain

---
+++ src/NewBranchPackage/NewMain.java
@@ -0,0 +1,10 @@
+package NewBranchPackage;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        NewBranchClass newBranchClass = new NewBranchClass();
+        newBranchClass.someCoolMethod();
+    }
+
+}
